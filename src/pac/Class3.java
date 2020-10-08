eefa9a6deab9425fcbb151cc1569f974816825d6
chandrakumarhs <hschandra655@gmail.com>
2020-10-08 11:47:46 +0530

    commit class 3 added newely

---
+++ src/pac/Class3.java
@@ -0,0 +1,12 @@
+package pac;
+
+import java.nio.charset.MalformedInputException;
+
+public class Class3
+{
+	public static void main(String[] args) {
+		System.out.println("Class 3 added newely");
+	}
+
+
+}
